f43ea4f7f7335e713ab58da0c08bc70ce246ffab
Denis Suprun <suprundenis@gmail.com>
2023-10-31 14:02:14 +0300

    + Main Page/VM

--- BasicApp-SwiftUI/MainPage.swift
+++ BasicApp-SwiftUI/MainPage.swift
@@ -5,4 +5,35 @@
 //  Created by Denis Suprun on 31.10.2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainPage<CD: MainViewModelCoordinatorDelegate, VM: MainViewModel<CD>>: View {
+
+    @StateObject var viewModel: VM
+    
+    var body: some View {
+        VStack(spacing: 32) {
+            Text("Counter: \(viewModel.counter)")
+            
+            Button("Increase") {
+                viewModel.increase()
+            }
+            
+            Button("Next") {
+                viewModel.next(binding: $viewModel.counter)
+            }
+        }.navigationTitle("Main Page")
+    }
+}
+
+class MainViewModelCoordinatorDelegate_Preview: MainViewModelCoordinatorDelegate {
+    func didPressedNext(input: String, result: Binding<Int>) { }
+}
+
+struct MainPage_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            MainPage(viewModel: MainViewModel<MainViewModelCoordinatorDelegate_Preview>(service: MathService()))
+        }
+    }
+}
--- BasicApp-SwiftUI/MainViewModel.swift
+++ BasicApp-SwiftUI/MainViewModel.swift
@@ -5,4 +5,30 @@
 //  Created by Denis Suprun on 31.10.2023.
 //
 
-import Foundation
+import SwiftUI
+
+protocol MainViewModelCoordinatorDelegate: ObservableObject {
+    func didPressedNext(input: String, result: Binding<Int>)
+}
+
+class MainViewModel<CD: MainViewModelCoordinatorDelegate>: ObservableObject {
+    
+    var service: MathService
+    
+    var coordinatorDelegate: ObservedObject<CD>? = nil
+    
+    @Published private var someText: String = "Some Main Page Text"
+    @Published var counter: Int = 0;
+        
+    init(service: MathService) {
+        self.service = service
+    }
+    
+    func next(binding: Binding<Int>) {
+        coordinatorDelegate?.wrappedValue.didPressedNext(input: "Counter increased to: \(counter)", result: binding)
+    }
+    
+    func increase() {
+        counter = service.increase(value: counter)
+    }
+}
