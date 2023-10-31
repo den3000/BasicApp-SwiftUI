a2686d67c240a4ddf24a83674026af2f99a15918
Denis Suprun <suprundenis@gmail.com>
2023-10-31 13:22:52 +0300

    Initial Commit

---
+++ BasicApp-SwiftUI/BasicApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicApp_SwiftUIApp.swift
+//  BasicApp-SwiftUI
+//
+//  Created by Denis Suprun on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
